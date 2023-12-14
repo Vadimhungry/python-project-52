17b421acbac56897e28ff37345be1bb18d006377
VadimK <yaralavadim@gmail.com>
2023-12-14 16:08:32 +0200

    create user sketch

--- task_manager/models.py
+++ task_manager/models.py
@@ -2,11 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(models.Model):
-    nickname = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.nickname
+        return self.username
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import CreateUserForm
-
+# from .forms import CreateUserForm
+from .forms import CustomUserCreationForm
+from .models import User
 
 def index(request):
     return render(request, "index.html", context={})
 
 
 def users(request):
-    users = CustomUser.objects.all()
+    users = User.objects.all()
     return render(
         request,
         "users.html",
@@ -19,15 +20,17 @@ def users(request):
 
 class UserCreateFormView(View):
     def get(self, request, *args, **kwargs):
-        form = CreateUserForm()
+        form = CustomUserCreationForm()
         return render(request, 'create_user.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
-        form = CreateUserForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
+        print(request.POST)
+        print(f'form is valid: ', form.is_valid())
         if form.is_valid():
             form.save()
             return redirect('create_user')
-        return render(request, 'articles/create.html', {'form': form})
+        return render(request, 'create_user.html', {'form': form})
 
 
 # def create_user(request):
